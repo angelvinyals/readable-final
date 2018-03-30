c7c8e8d5901b5547eba6d412d43cbe5c4519b8a6
angelvinyals <angel.vinyals@gmail.com>
2018-03-30 21:03:01 +0200

    API-SERVER added

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,18 @@
+const url= 'http://localhost:3001'
+
+const headers = {
+  Authorization: 'some-token',
+  'content-type': 'application/json',
+  'cache-control': 'no-cache',
+};
+
+export const getCategories = () =>
+  fetch(`${url}/categories`, {
+    method: 'GET',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => res.json())
+    .then(data => data.categories);
