74c567b5d959cdeb4c4817a19ec28f493aa2a9ed
angelvinyals <angel.vinyals@gmail.com>
2018-03-31 19:38:46 +0200

    Updtaed HEADER component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
 
 const Header = () => (
   <div className="containerFlex">
-    <div className = "row one_row H_100 H_center V_center">
       <h4 className="textCenter">READABLE</h4>
-    </div>
   </div>
 );
-
-Header.PropTypes={
-  Header: PropTypes.func.isRequired
-}
 
 export default Header;
