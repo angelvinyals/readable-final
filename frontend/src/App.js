859f885a2c1037d5c0deee04c6997da3f8b450b9
angelvinyals <angel.vinyals@gmail.com>
2018-04-12 02:47:20 +0200

    added react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+//import PropTypes from 'prop-types';
+//import { connect } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 //import { fetchCategories } from './actions/categoriesActions';
@@ -50,95 +50,5 @@ class App extends Component {
   }
 }
 
-<<<<<<< HEAD
-App.propTypes ={
-  getCategories: PropTypes.func.isRequired
-}
-=======
-          <ul>
-            {categories.length > 0 && categories.map(cat => (
-              <li key={cat.path}>
-                {cat.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    />
-    <Posts
-      url="http://localhost:3001/posts"
-      render={({ posts, isLoading }) => (
-        <div>
-          <h2>Posts</h2>
-          {isLoading && <h2>Loading...</h2>}
-          <ul>
-            {Object.keys(posts).length>0 &&
-              Object.keys(posts)
-                .map(key => posts[key])
-                .map(p => (
-                  <li key={`p${p.id}`}>
-                    <ul>
-                      {Object.keys(p).map(k =>(
-                          <li key={`${p.id}-${k}`}>
-                            {`${k}: ${p[k]}`}
-                          </li>
-                      ))}
-                    </ul>
-                    <hr/>
-                  </li>
-                ))
-            }
-          </ul>
-        </div>
-      )}
-    />
-    <button >
-      add a post
-    </button>
-    <div className="container">
-      <Form
-        categories={['a props','bb props','ccc props']}
-        render={({ categories, isLoading }) => (
-          <div>
-            <div>
-              <label>
-                Title:
-                <input type="text" name="title" onChange={this.handleChange} />
-              </label>
-            </div>
-            <div>
-              <label>
-                Post:
-                <textarea name="body" onChange={this.handleChange} />
-              </label>
-            </div>
-            <div>
-              <label>
-                Author:
-                <input type="text" name="author" onChange={this.handleChange} />
-              </label>
-            </div>
-            <div>
-              <label>
-                Pick your category:
-                <select name="category" onChange={this.handleChange}>
-                  {['p','q'].map(cat => <option value={cat} key={`option${cat}`}>{cat}</option>)}         
-                </select>
-              </label>
-            </div>
-            <input type="submit" value="add a post" />
 
-          </div>
-        )}
-      />
-    </div>
-  </div>
-)
->>>>>>> 96d5eacef8bf0131184353f0866dbc94998ad119
-
-const mapDispatchToProps = dispatch => ({
-  //getCategories: () => dispatch(fetchCategories()),
-  //getPosts: () => dispatch(fetchPosts()),
-});
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
